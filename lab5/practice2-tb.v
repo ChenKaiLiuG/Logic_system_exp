14a6e4a888a43b9f6e72376f79a89b05e98a584b
ChenKaiLiuG <141424456+ChenKaiLiuG@users.noreply.github.com>
2025-04-28 00:32:21 +0800

    Update practice2-tb.v
    刪除註解

--- lab5/practice2-tb.v
+++ lab5/practice2-tb.v
@@ -8,7 +8,6 @@ module Marquee_tb;
     wire led4_g;
     wire led5_g;
 
-    // 實例化待測試的 Marquee 模組
     Marquee uut (
         .clk(clk),
         .rst(rst),
@@ -17,23 +16,20 @@ module Marquee_tb;
         .led5_g(led5_g)
     );
 
-    // 時脈產生
-    parameter CLK_PERIOD = 8; // 對應 125MHz 時脈 (1 / 125e6 = 8ns)
+    parameter CLK_PERIOD = 8; // 125MHz (1 / 125,000,000 = 8ns)
     initial begin
         clk = 0;
         forever #(CLK_PERIOD / 2) clk = ~clk;
     end
 
-    // 重置訊號產生
     initial begin
         rst = 1;
-        #10; // 保持重置 10 ns
+        #10; // rst 10 ns
         rst = 0;
-        #1000000; // 模擬一段時間 (1 ms)
+        #10000000000; 
         $finish;
     end
 
-    // 監測輸出訊號
     initial begin
         $monitor("Time=%t, rst=%b, led=%b, led4_g=%b, led5_g=%b", $time, rst, led, led4_g, led5_g);
     end
