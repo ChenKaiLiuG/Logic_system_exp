aa882f14360cdb1159a621f6af440c406e315b6c
ChenKaiLiuG <141424456+ChenKaiLiuG@users.noreply.github.com>
2025-04-28 15:48:26 +0800

    Update practice1-tb.v

--- lab5/practice1-tb.v
+++ lab5/practice1-tb.v
@@ -2,37 +2,31 @@
 
 module Marquee_tb;
 
-    // 宣告測試模組的訊號
     reg clk;
     reg rst;
     wire [3:0] led;
 
-    // 實例化被測試的 Marquee 模組
     Marquee uut (
         .clk(clk),
         .rst(rst),
         .led(led)
     );
+    
+    localparam CLK_PERIOD = 8; // 125MHz ，1/125,000,000 = 8 ns
 
-    // 定義時脈週期
-    localparam CLK_PERIOD = 8; // 對應 125MHz 時脈，週期為 1/125e6 秒 = 8 ns
-
-    // 產生時脈訊號
     initial begin
         clk = 0;
         forever #(CLK_PERIOD/2) clk = ~clk;
     end
 
-    // 產生重置訊號
     initial begin
         rst = 1;
-        #10; // 在開始後的 20 ns (幾個時脈週期) 將重置訊號拉低
+        #10; 
         rst = 0;
-        #100000; // 模擬一段時間 (200 ms) 以觀察跑馬燈效果
-        $finish; // 結束模擬
+        #100000; 
+        $finish; 
     end
 
-    // 監測 LED 輸出的變化
     initial begin
         $monitor("Time = %t, rst = %b, state = %b, counter = %d, uut.led = %b, uut.led_sel = %b",
         $time, rst, uut.state, uut.counter, uut.led, uut.led_sel);
