7aaf0f921177de59bbf674cebc704f9701f536f1
ChenKaiLiuG <141424456+ChenKaiLiuG@users.noreply.github.com>
2025-04-29 16:56:39 +0800

    Update practice1.v

--- lab6/practice1.v
+++ lab6/practice1.v
@@ -9,15 +9,15 @@ reg [7:0] phase_counter;
 reg [7:0] bright_threshold;
 reg [16:0] timer_8ns;
 reg [9:0] timer_1ms;
-reg [2:0] color_idx; // ?”¨ä¾†è¼ªæµå?‡æ?›ä?ƒå?‹é?è‰²
+reg [2:0] color_idx; // 用來輪流切換七個顏色
 
 //reg [9:0] counter_0;
 
 wire passed_1ms = (timer_8ns == `CYCLE_1MS);
-wire passed_10ms = passed_1ms && (timer_1ms % 10 == 9); // æ¯? 10ms ?²è?Œä?æ¬¡æ›´?–°
+wire passed_10ms = passed_1ms && (timer_1ms % 10 == 9); // 每 10ms 進行一次更新
 wire acc_999ms = (timer_1ms == 10'd999);
-
-// é¡è‰²å°æ?‰ç?„äº®åº¦æ?”ä?‹ï?ˆæ?é«˜äº®åº¦ç‚º 100ï¼?
+    
+// 顏色對應的亮度比例（最高亮度為 100）
 reg [7:0] target_r, target_g, target_b;
 always @(*) begin
     case (color_idx)
@@ -31,21 +31,21 @@ always @(*) begin
         default: begin target_r=0; target_g=0; target_b=0; end
     endcase
 end
-
-// ?Ÿº?œ¬ 1ms è¨ˆæ?‚å™¨
+    
+// 基本 1ms 計時器
 always @(posedge clk or posedge rst) begin
     if (rst) timer_8ns <= 0;
     else if (passed_1ms) timer_8ns <= 0;
     else timer_8ns <= timer_8ns + 1;
 end
 
-// 1ms ?–®ä½ç?? timer
+// 1ms 單位的 timer
 always @(posedge clk or posedge rst) begin
     if (rst) timer_1ms <= 0;
     else if (passed_1ms) timer_1ms <= acc_999ms ? 0 : timer_1ms + 1;
 end
 
-// ?‘¼?¸äº®åº¦è®Šå?–ï??0 ~ 100 ??å?žä??
+// 呼吸亮度變化：0 ~ 100 再回來
 always @(posedge clk or posedge rst) begin
     if (rst) bright_threshold <= 0;
     else if (passed_10ms) begin
@@ -53,14 +53,14 @@ always @(posedge clk or posedge rst) begin
         else bright_threshold <= bright_threshold - 1;
     end
 end
-
-// é¡è‰²è¼ªæ›¿ï¼šæ?ç?’å?‡æ?›ä?æ¬¡é?è‰²
+    
+// 顏色輪替：每秒切換一次顏色
 always @(posedge clk or posedge rst) begin
     if (rst) color_idx <= 0;
     else if (acc_999ms) color_idx <= (color_idx == 6) ? 0 : color_idx + 1;
 end
 
-// ?‘¼?¸PWM?Ž§?ˆ¶
+// 呼吸PWM控制
 always @(posedge clk or posedge rst) begin
     if (rst) phase_counter <= 0;
     else if (phase_counter == 199) phase_counter <= 0;
